40372bae7816785e3aff6ba56a185ec52b1e8e6d
TheSillyP <Petjenorden@gmail.com>
2024-07-29 09:33:08 +0200

    Created Spring boot project

---
+++ src/main/java/com/tunefusion/tunefusion/TuneFusionApplication.java
@@ -0,0 +1,13 @@
+package com.tunefusion.tunefusion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuneFusionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TuneFusionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tunefusion/tunefusion/TuneFusionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tunefusion.tunefusion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TuneFusionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
